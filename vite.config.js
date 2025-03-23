599a9faa9962105f6c9512794a68c94a13e806fc
Nagmani Jha <nagmanijha31@gmail.com>
2025-03-23 23:12:12 +0530

    Fix MIME type issue

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  tailwindcss()],
-  resolve: {
-    alias: {
-      // Ensure there are no conflicting aliases
-    },
-  },
-})
+  plugins: [react()],
+  base: '/', // Ensure this is correct for your project
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
+});
